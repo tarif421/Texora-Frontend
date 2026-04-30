881f083d904ee7b4cfe0e61d73e3edfc290db3fa
Tariful Hasan <tarif305@gmail.com>
2026-04-30 21:17:56 +0600

    react hook added

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const {} = useForm();
   return (
-    <div className="min-h-screen flex items-center justify-center" >
-      <form   >
+    <div className="min-h-screen flex items-center justify-center bg-base-200 p-4">
+      <form className="card bg-base-100 w-full max-w-sm shadow-2xl p-8">
         <fieldset className="fieldset">
           <label className="label">Email</label>
           <input type="email" className="input" placeholder="Email" />
