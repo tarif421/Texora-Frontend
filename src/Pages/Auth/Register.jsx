ff15c2be05432daf2ed2d0363cea382fbdaab5c7
Tariful Hasan <tarif305@gmail.com>
2026-05-01 12:07:49 +0600

    register page on progress

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -2,15 +2,59 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const Register = () => {
-  const {} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegistration = (data) => {
+    console.log("fter register", data);
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-base-200 p-4">
-      <form className="card bg-base-100 w-full max-w-sm shadow-2xl p-8">
+      <form
+        onSubmit={handleSubmit(handleRegistration)}
+        className="card bg-base-100 w-full max-w-sm shadow-2xl p-8"
+      >
         <fieldset className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is Required</p>
+          )}
+          {/* password */}
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              pattern:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password must be 6 characters or longer
+            </p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500">
+              Password must have one uppercase, one lowercase and a special
+              character
+            </p>
+          )}
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
