499d4be550761f9527448a31cebcf837a995a354
Tariful Hasan <tarif305@gmail.com>
2026-05-02 20:35:01 +0600

    login functionality on progress

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,22 +1,56 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import useAuth from "../../Hooks/useAuth";
 
 const Login = () => {
-  const { register, handleSubmit , formState: { errors }} = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { signInUser } = useAuth();
 
-  const handleRegistration = (data) => {
-console.log("after register", data)
-  }
+  const handleLogin = (data) => {
+    console.log("after register", data);
+    signInUser(data.email, data.password)
+    .then(result => {
+      console.log(result.user)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  };
   return (
     <div className=" flex items-center justify-center bg-base-200 p-4  min-h-screen">
-      <form onSubmit={handleSubmit(handleRegistration)} className="card bg-base-100  w-full max-w-sm shadow-2xl p-8">
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="card bg-base-100  w-full max-w-sm shadow-2xl p-8"
+      >
         <fieldset className="fieldset">
           {/* email */}
           <label className="label">Email</label>
-          <input {...register('email', {required: true})} type="email" className="input" placeholder="Email" />
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required</p>
+          )}
           {/* password */}
           <label className="label">Password</label>
-          <input {...register('password')} type="password" className="input" placeholder="Password" />
+          <input
+            {...register("password", { required: true, minLength: 6 })}
+            type="password"
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password must be 6 character of longer
+            </p>
+          )}
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
