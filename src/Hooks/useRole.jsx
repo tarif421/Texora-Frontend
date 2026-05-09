7e0c05c89a6c6bab278217fa4f7f14983b4e6acb
Tariful Hasan <tarif305@gmail.com>
2026-05-09 19:54:05 +0600

    manage ueses page on progress

--- src/Hooks/useRole.jsx
+++ src/Hooks/useRole.jsx
@@ -1,20 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useRole = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-
-  const { isLoading, data: role = "user" } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["user-role", user?.email],
+
+    enabled: !!user?.email,
     queryFn: async () => {
       const res = await axiosSecure.get(`/users/${user.email}/role`);
       return res.data;
     },
   });
-  return { role, isLoading };
+
+  return {
+    role: data?.role,
+    isLoading,
+  };
 };
 
 export default useRole;
